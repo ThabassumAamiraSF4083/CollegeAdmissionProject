0d3d933daba7d3492eeae590b314da952d21653b
AnirudhSF4051 <121146541+AnirudhSF4051@users.noreply.github.com>
2022-12-23 22:08:29 +0530

    Updated admission details class

--- ApplicationStudentAdmission/AdmissionDetails.cs
+++ ApplicationStudentAdmission/AdmissionDetails.cs
@@ -1,12 +1,30 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace ApplicationStudentAdmission
-{ 
+{
+    public enum AdmissionStatus{Select,Admitted,Cancelled}
     public class AdmissionDetails
-    { 
-      
+    {
+        public static int s_admissionID=1001;
+        public string AdmissionID { get;}
+        public string StudentID { get; set; }
+        public string DepartmentID { get; set; }
+
+        public static int s_studentID;
+        public static int s_departmentID;
+        public DateTime AdmissionDate { get;}
+        public AdmissionStatus AdmissionStatus { get; set; }
+
+        public AdmissionDetails(string studentID,string departmentID,DateTime admissiondate,AdmissionStatus admissionstatus)
+        {
+            s_admissionID++;
+            AdmissionID="AID"+s_admissionID;
+            StudentID=studentID;
+            DepartmentID=departmentID;
+            
+            
+            AdmissionDate=admissiondate;
+            AdmissionStatus=admissionstatus;
+        }
+
     }
 }
